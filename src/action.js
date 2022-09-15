94feffc108a004530d5498d26acfd1894105fbd5
Aaron Ford <aaronford@cordel.ai>
2022-09-15 14:37:55 +1000

    feat(action): Adds comment to new pull requests

--- src/action.js
+++ src/action.js
@@ -1,5 +1,21 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
 async function run() {
-  console.log('Hello World!');
+  const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+  const comment = process.env.COMMENT ?? 'Default Comment.';
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+
+  const { pull_request } = context.payload;
+
+  await octokit.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: `${comment}`,
+  });
 }
 
 run();
